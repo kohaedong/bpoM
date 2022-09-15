11bab20d40357c62fa50939ba57f2cc0caa750b6
bakbeom <bakbeom@bakbeom.local>
2022-09-15 13:40:47 +0900

    clean

--- lib/view/orderManager/provider/order_manager_page_provider.dart
+++ lib/view/orderManager/provider/order_manager_page_provider.dart
@@ -2,7 +2,7 @@
  * Project Name:  [mKolon3.0] - MedicalSalesPortal
  * File: /Users/bakbeom/work/sm/si/medsalesportal/lib/view/orderManager/provider/order_manager_page_provider.dart
  * Created Date: 2022-07-05 09:57:03
- * Last Modified: 2022-09-15 13:11:39
+ * Last Modified: 2022-09-15 13:40:27
  * Author: bakbeom
  * Modified By: bakbeom
  * copyright @ 2022  KOLON GROUP. ALL RIGHTS RESERVED. 
@@ -12,7 +12,6 @@
  */
 
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
 import 'package:medsalesportal/util/date_util.dart';
 import 'package:medsalesportal/util/encoding_util.dart';
 import 'package:medsalesportal/enums/request_type.dart';
