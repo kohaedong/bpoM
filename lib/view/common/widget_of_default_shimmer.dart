e734802e1f2bf643ee335fbbe00fc498a39a7b93
bakbeom <bakbeom@bakbeom.local>
2022-10-14 18:25:22 +0900

    clean!~

--- lib/view/common/widget_of_default_shimmer.dart
+++ lib/view/common/widget_of_default_shimmer.dart
@@ -2,7 +2,7 @@
  * Project Name:  [mKolon3.0] - SalesPortal
  * File: /Users/bakbeom/work/sm/si/SalesPortal/lib/view/common/default_shimmer.dart
  * Created Date: 2021-10-13 10:00:13
- * Last Modified: 2022-10-14 17:06:05
+ * Last Modified: 2022-10-14 18:24:31
  * Author: bakbeom
  * Modified By: bakbeom
  * copyright @ 2022  KOLON GROUP. ALL RIGHTS RESERVED. 
@@ -12,81 +12,15 @@
  */
 
 import 'dart:math';
-
+import 'base_shimmer.dart';
 import 'package:flutter/material.dart';
-import 'package:medsalesportal/styles/app_colors.dart';
 import 'package:medsalesportal/styles/app_size.dart';
 import 'package:medsalesportal/styles/app_style.dart';
+import 'package:medsalesportal/styles/app_colors.dart';
 import 'package:medsalesportal/styles/app_text_style.dart';
 import 'package:medsalesportal/view/common/widget_of_default_spacing.dart';
-import 'package:medsalesportal/view/common/widget_of_divider_line.dart';
-
-import 'base_info_row_by_key_and_value.dart';
-import 'base_shimmer.dart';
 
 class DefaultShimmer {
-  // static Widget buildDefaultPageShimmer(int length,
-  //     {bool? isNotWithPadding,
-  //     bool? isWithSet,
-  //     int? setLenght,
-  //     bool? isWithBlodDiveder}) {
-  //   return isWithSet != null
-  //       ? ListView(
-  //           shrinkWrap: true,
-  //           children: [
-  //             Column(
-  //               mainAxisAlignment: MainAxisAlignment.center,
-  //               children: [
-  //                 ...List.generate(
-  //                   setLenght!,
-  //                   (index) => Padding(
-  //                     padding: EdgeInsets.all(isNotWithPadding != null
-  //                         ? AppSize.zero
-  //                         : AppSize.padding),
-  //                     child: Column(
-  //                       mainAxisAlignment: MainAxisAlignment.center,
-  //                       children: [
-  //                         ...List.generate(
-  //                             length,
-  //                             (index) => BaseInfoRowByKeyAndValue.build('', '',
-  //                                 isWithShimmer: true)),
-  //                         Divider()
-  //                       ],
-  //                     ),
-  //                   ),
-  //                 ),
-  //                 isWithBlodDiveder != null ? buildDividingLine() : Container()
-  //               ],
-  //             )
-  //           ],
-  //         )
-  //       : ListView(
-  //           shrinkWrap: true,
-  //           children: [
-  //             Column(
-  //               mainAxisAlignment: MainAxisAlignment.center,
-  //               children: [
-  //                 Padding(
-  //                   padding: EdgeInsets.all(isNotWithPadding != null
-  //                       ? AppSize.zero
-  //                       : AppSize.padding),
-  //                   child: Column(
-  //                     mainAxisAlignment: MainAxisAlignment.center,
-  //                     children: [
-  //                       ...List.generate(
-  //                           length,
-  //                           (index) => BaseInfoRowByKeyAndValue.build('', '',
-  //                               isWithShimmer: true))
-  //                     ],
-  //                   ),
-  //                 ),
-  //                 buildDividingLine(),
-  //               ],
-  //             )
-  //           ],
-  //         );
-  // }
-
   static buildDefaultResultShimmer({bool? isNotPadding}) {
     return BaseShimmer(
       child: ListView.builder(
